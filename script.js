d5ef2e89c43ee134dcbd9f112ff492938f54464f
Mikael Larsson <micke.lar@gmail.com>
2022-01-19 10:55:42 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,60 @@
+const btn = document.querySelector('#searchBtn');
+btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    getApi();
+});
+
+const query = document.querySelector('#swInput');
+const starWarText = document.querySelector('#starWarText');
+
+function getApi() {
+    const fullUri = 'https://www.swapi.tech/api/people?name=' + query.value.trim();
+
+    fetch(fullUri)
+        .then(res => res.json())
+        .then(data => {
+            console.log('Data: ' + data.result)
+            //console.log(data.result[0].properties.height);
+
+            if (data.result !== undefined && data.result.length > 0){
+                starWarText.value = '';
+
+                data.result.forEach(element => {
+                    // starWarText.value += 'Desription: ' + element.description;
+                    // starWarText.value += ('\nNamn: ' + element.properties.name);
+                    // starWarText.value += ('\nLängd: ' + element.properties.height);
+                    // starWarText.value += ('\nFödelseår: ' + element.properties.birth_year);
+                    // starWarText.value += ('\nVikt: ' + element.properties.mass);
+                    // starWarText.value += ('\nKön: ' + element.properties.gender);
+                    // starWarText.value += ('\nHårfärg: ' + element.properties.hair_color);
+
+
+                    let swt =
+                        `Description: ${element.description}, 
+Namn: ${element.properties.name}
+Längd: ${element.properties.height},
+Födelseår: ${element.properties.birth_year},
+Vikt: ${element.properties.mass},
+Kön: ${element.properties.gender},
+Hårfärg: ${element.properties.hair_color}`;
+
+                    starWarText.value = swt;
+
+                    console.log(swt);
+                    // console.log('Desription: '+ element.description);
+                    // console.log('Name: '+ element.properties.name);
+                    // console.log('Height: '+ element.properties.height);
+                    // console.log('Birth year: '+ element.properties.birth_year);
+                });
+            }
+            else {
+                starWarText.value = 'Hittade inte.';
+
+            }
+        })
+        .catch(err => console.log('Error ' + err));
+
+
+
+
+}
